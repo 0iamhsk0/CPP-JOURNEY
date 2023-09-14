112875a7c3cd9e3a37125b780de0bb5574952749
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2023-09-14 23:39:56 +0530

    Add files via upload

--- find.cpp
+++ find.cpp
@@ -5,9 +5,13 @@ int main()
 {
    string s1 = "arlcome";
    string s2 = "wearomea";
-   cout << s1.find("lc") << endl;  // o/p is 2
-   cout << s2.rfind("ea") << endl; // o/p is 6
-   cout << s2.find_last_of("e");   // o/p is 6
+   cout << s1.find("lc") << endl;         // o/p is 2
+   cout << s2.rfind("ea") << endl;        // o/p is 6
+   cout << s2.find_last_of("e") << endl;  // o/p is 6
+   cout << s2.find_first_of("w") << endl; // o/p is 0
+
    // if anything other is seached garbage value will come
+
+   cout << s2.find_last_of("1");
    return 0;
 }
